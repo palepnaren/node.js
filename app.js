845f877d80e51c519c72ac7b960a186189c77646
Naren <palepnaren@gmail.com>
2016-08-28 19:54:00 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/',function(request, response){
+  response.sendFile(__dirname + "/index.html");
+});
+
+var port = process.env.PORT=4000;
+var server = app.listen(port,function(req,res){
+  console.log("server started at port" +port);
+});
